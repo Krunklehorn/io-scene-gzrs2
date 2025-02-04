717d9909f2f1c17a0bbc72a7d6e769ca90e29a7f
Krunklehorn <krunklehornvector@gmail.com>
2025-02-04 11:34:28 -0500

    Change light default end attenuation to 10

--- __init__.py
+++ __init__.py
@@ -2978,7 +2978,7 @@ class GZRS2LightProperties(PropertyGroup):
     attEnd: FloatProperty(
         name = 'Attenuation End',
         default = 0.0,
-        min = 0.0,
+        min = 10.0,
         max = 3.402823e+38,
         soft_min = 0.0,
         soft_max = 3.402823e+38,
