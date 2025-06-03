71b51e35fae38fe72a2269564620dfbd635f9dac
Krunklehorn <krunklehornvector@gmail.com>
2025-06-03 11:53:05 -0400

    Formatting

--- lib/lib_gzrs2.py
+++ lib/lib_gzrs2.py
@@ -2271,7 +2271,7 @@ def createColTriangles(polygons):
 
         for _, _, vertex1, vertex2 in vertexPairs:
             if vec3IsClose(vertex1, vertex2, RS_COORD_THRESHOLD):
-                raise GZRS2DegeneratePolygonError("GZRS2: createColTriangles() found a degenerate polygon! Try running Cleanup->Split Concave Faces or turn on the Mesh Analyzer and set it to \"Intersect\"!")
+                raise GZRS2DegeneratePolygonError("GZRS2: createColTriangles() found a degenerate polygon! Try using the \"Pre-process Geometry\" operation or turn on the Mesh Analyzer and set it to \"Intersect\" to search for issues!")
 
     for polygon in polygons:
         for v in range(polygon.vertexCount - 2):
